c4470d76609d4b77e50a260ddb28f13562128e39
BrnMarq <brnmarq@gmail.com>
2024-04-29 19:13:50 -0400

    ✨ Added RenderUtilities file

---
+++ src/lib/include/RenderUtilities.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <Card.hpp>
+
+std::string get_card_image(const Card& card);
---
+++ src/lib/src/RenderUtilities.cpp
@@ -0,0 +1,5 @@
+#include <RenderUtilities.hpp>
+
+std::string get_card_image(const Card& card) {
+  return "asdfasdfa";
+};
