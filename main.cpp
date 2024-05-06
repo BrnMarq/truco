5b87d58bdc4abfcf303f9183659ec148432b7a21
BrnMarq <brnmarq@gmail.com>
2024-05-06 11:15:43 -0400

    🍱 Update images to make them all the same size

--- main.cpp
+++ main.cpp
@@ -5,23 +5,18 @@
 
 int main(int , char *[])
 {
-    std::list<int> t;
-    auto y = t.begin();
-    rng_t mt(time(nullptr));
-    CardDeck deck = CardDeck::create_random_unique_deck(mt, 3, true);
     auto guiFunction = [&]() {
-        ImVec2 vec(0, 0);
-        ImVec2 vec2(1, 1);
-        ImVec2 vec3(1, 0);
-        ImVec2 vec4(0, 1);
+        ImVec2 vec(100.0F, 153.5F);
+        rng_t mt(time(nullptr));
+        CardDeck deck = CardDeck::create_random_unique_deck(mt, 3, true);
 
         for (const auto& card: deck) {
-            HelloImGui::ImageFromAsset(get_card_image(card).c_str()); ImGui::SameLine();
+            HelloImGui::ImageFromAsset(get_card_image(card).c_str(), vec); ImGui::SameLine();
         }
 
         if (ImGui::Button("Bye!"))
             HelloImGui::GetRunnerParams()->appShallExit = true;
     };
-    HelloImGui::Run(guiFunction, Settings::hand_size == 8 ? "Truquito" : "Truco");
+    HelloImGui::Run(guiFunction, "Truco Venezolano");
     return 0;
 }
